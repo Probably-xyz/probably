eeaa366c482c9e76de29069d40ee1cce754ff1b7
NBK-01 <nayefkanaan03@gmail.com>
2024-01-16 18:52:47 +0200

    Change font to DM-Sans

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,12 +1,11 @@
-import {Ubuntu, Inter} from "next/font/google"
+import {DM_Sans, Inter} from "next/font/google"
 
 const inter = Inter({
     subsets: ["latin"],
   });
 
-const ubuntu = Ubuntu({
-    subsets: ["latin"],
-    weight: "500"
+const ubuntu = DM_Sans({
+    subsets: ["latin"]
 });
 
 
