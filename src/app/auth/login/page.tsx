14d307d4ccb030a85b1874fbd1ceaf44a55337ee
NBK-01 <nayefkanaan03@gmail.com>
2024-01-17 18:23:19 +0200

    auth: initial auth setup

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
+const SignInButton = () => {
+  return (
+    <>
+     <button
+      className="bg-slate-600 px-4 py-2 text-white"
+      onClick={() => signIn("github", { callbackUrl: "/manifesto" })}
+      type="button"
+    >
+      Sign In With GitHub
+    </button>
+    </>
+  );
+};
+
+export default SignInButton;
