38db1e29c53abd933177a9899df459fda55470b2
NBK-01 <nayefkanaan03@gmail.com>
2024-01-19 17:28:20 +0200

    wip

--- src/_components/auth-comps.tsx
+++ src/_components/auth-comps.tsx
@@ -1,23 +1,33 @@
 "use client"
-
 import React from 'react'
 import { signOut } from "next-auth/react"
 import { Button } from '~/styles/ui/button'
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { Input } from '~/styles/ui/input';
 
 export const GoogleBtn = () => {
   return (
-    <div>Google</div>
+    <Button className="w-full" size="lg">
+     <FaGoogle className="mr-2 h-4 w-4 my-auto"/>
+      with Google
+    </Button>
   )
 }
 
 export const GithubBtn = () => {
   return (
-    <Button>
-      Signin with Github
+    <Button className="w-full" size="lg">
+      <FaGithub className="mr-2 h-4 w-4"/>
+       with Github
     </Button>
   )
 }
 
+export const SigninEmail = () => {
+    return (
+      <Input placeholder='Sign in with email' className="bg-white"/>    
+    )
+}
 export const LogoutBtn = () => {
   return (
     <Button onClick={() => signOut()}>
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,29 @@
-import Link from "next/link";
+import { GithubBtn, GoogleBtn, SigninEmail } from "~/_components/auth-comps";
+import { GridGradient } from "~/_components/bg-comps";
+import { ubuntu } from "~/lib/fonts";
 import { getServerAuthSession } from "~/server/auth";
-import { Button } from "~/styles/ui/button";
 
 export default async function SignInButton() {
   const session = await getServerAuthSession()
   return (
     <>
-      {session ? <Button> Logout </Button> : <Link href="/api/auth/signin"> Signin </Link>}
-      {session ? <p> {session?.user.email} </p> : <p> Not signed in </p>}
+      <GridGradient/>
+      <div className="container flex flex-col mx-auto items-center justify-center min-h-[75vh]">
+          <div className="justify-center items-center">
+              <h1 className="text-4xl text-center mb-10" style={ubuntu.style}>
+                Login to Probably
+              </h1>
+              
+              <div className="w-[400px] space-y-4 flex flex-col">
+                <SigninEmail/>
+                <div className="w-[350px border-t border-neutral-900 items-center justify-center">
+                </div>
+                <span className='mx-auto'> or </span>
+                <GoogleBtn/>
+                <GithubBtn/>
+              </div>
+          </div>
+      </div>
     </>
   );
 };
