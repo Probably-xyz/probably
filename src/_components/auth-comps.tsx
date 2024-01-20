07b3d3d9144dd59ac59dc83bd54435fc512b509c
NBK-01 <nayefkanaan03@gmail.com>
2024-01-20 16:57:38 +0200

    fix: dep updates

--- src/_components/auth-comps.tsx
+++ src/_components/auth-comps.tsx
@@ -4,6 +4,7 @@ import { signOut } from "next-auth/react"
 import { Button } from '~/styles/ui/button'
 import { FaGoogle, FaGithub } from "react-icons/fa";
 import { Input } from '~/styles/ui/input';
+import { Label } from '~/styles/ui/label';
 
 export const GoogleBtn = () => {
   return (
@@ -16,9 +17,9 @@ export const GoogleBtn = () => {
 
 export const GithubBtn = () => {
   return (
-    <Button className="w-full" size="lg">
+    <Button className="w-full" size="lg" variant="outline">
       <FaGithub className="mr-2 h-4 w-4"/>
-       with Github
+      Github
     </Button>
   )
 }
@@ -33,5 +34,47 @@ export const LogoutBtn = () => {
     <Button onClick={() => signOut()}>
       Logout
     </Button>
+  )
+}
+
+export const AuthForm = () => {
+  return (
+    <div className="grid gap-6">
+    <div className="grid gap-2">
+    <div className="grid gap-1">
+      <Label className="sr-only" htmlFor="email">
+        Email
+      </Label>
+      <Input
+        id="email"
+        placeholder="name@example.com"
+        type="email"
+        autoCapitalize="none"
+        autoComplete="email"
+        autoCorrect="off"
+       
+      />
+      {/* {errors?.email && (
+        <p className="px-1 text-xs text-red-600">
+          {errors.email.message}
+        </p>
+      )} */}
+    </div>
+    <Button>
+      Sign In with Email
+    </Button>
+  </div>
+  <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+     <GithubBtn/>
+    </div>
   )
 }
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
-import { GithubBtn, GoogleBtn, SigninEmail } from "~/_components/auth-comps";
-import { GridGradient } from "~/_components/bg-comps";
+import { AuthForm, GithubBtn, GoogleBtn, SigninEmail } from "~/_components/auth-comps";
+import { GridGradient, HalfGrid } from "~/_components/bg-comps";
 import { ubuntu } from "~/lib/fonts";
 import { getServerAuthSession } from "~/server/auth";
 
@@ -7,21 +7,22 @@ export default async function SignInButton() {
   const session = await getServerAuthSession()
   return (
     <>
-      <GridGradient/>
+      <HalfGrid/>
       <div className="container flex flex-col mx-auto items-center justify-center min-h-[75vh]">
           <div className="justify-center items-center">
               <h1 className="text-4xl text-center mb-10" style={ubuntu.style}>
                 Login to Probably
               </h1>
               
-              <div className="w-[400px] space-y-4 flex flex-col">
+              {/* <div className="w-[400px] space-y-4 flex flex-col">
                 <SigninEmail/>
                 <div className="w-[350px border-t border-neutral-900 items-center justify-center">
                 </div>
                 <span className='mx-auto'> or </span>
                 <GoogleBtn/>
                 <GithubBtn/>
-              </div>
+              </div> */}
+              <AuthForm/>
           </div>
       </div>
     </>
