9586cf2a5a01a1f92ecdd8f0bed405881bfb9982
NBK-01 <nayefkanaan03@gmail.com>
2024-01-22 17:41:54 +0200

    avatar dropdown

--- src/_components/dashboard.tsx
+++ src/_components/dashboard.tsx
@@ -105,24 +105,32 @@ export function SideBar() {
   )
 }
 
-export function UserAvatar() {
+interface UserDetails {
+  initials: string,
+  image: string,
+  name: string,
+  email: string,
+  id: string
+}
+
+export function UserAvatar({initials, image, name, email, id}: UserDetails) {
   return (
       <>
         <DropdownMenu>
       <DropdownMenuTrigger asChild className="my-auto">
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src={image} alt={name} />
+            <AvatarFallback> {initials} </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none"> {name} </p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {email}
             </p>
           </div>
         </DropdownMenuLabel>
