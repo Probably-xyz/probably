e904d7bf067e54f921fbe1fabb4378b67886d067
NBK-01 <nayefkanaan03@gmail.com>
2024-01-29 16:14:27 +0200

    Settings page

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TopDashNav } from '~/_components/nav'
+
+const Settings = () => {
+  return (
+    <>
+    <TopDashNav title="settings" />
+    <div>Settings</div>
+    </>
+  )
+}
+
+export default Settings
